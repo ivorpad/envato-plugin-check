649b6181df33fa9e556269f5d71e34b190314b85
Ivor Padilla <ivorjpc@gmail.com>
2019-06-11 17:45:47 +0200

    improved regex to find prepare methods without placeholders

--- checks/codecanyon.php
+++ checks/codecanyon.php
@@ -32,22 +32,22 @@ class CodeCanyon implements plugincheck {
 
 
 		$warn_checks = array(
-			'/@(?!media|keyframes|font)(\$|([a-zA-Z]))+/'     => esc_html__( 'Possible error suppression is being used', 'plugin-check' ),
-			'/@include/'                                      => esc_html__( 'Possible error suppression is being used', 'plugin-check' ),
-			'/@require/'                                      => esc_html__( 'Possible error suppression is being used', 'plugin-check' ),
-			'/@file/'                                         => esc_html__( 'Possible error suppression is being used', 'plugin-check' ),
-			'/balanceTags\s*\(\s*/'             						=> esc_html__( 'Possible data validation issues found. balanceTags() does not escape data', 'plugin-check' ),
-			'/force_balance_tags\s*\(\s*/'      						=> esc_html__( 'Possible data validation issues found. force_balance_tags() does not escape data', 'plugin-check' ),
-			'/(echo|print)\s*(\$|[a-zA-Z])/' 									=> esc_html__( 'Possible data validation issues found. All dynamic data must be correctly escaped for the context where it is rendered', 'plugin-check' ),
-			'/$_SERVER/'                   										=> esc_html__( 'PHP Global Variable found. Ensure the context is safe and reliable', 'plugin-check' ),
-			'/(?<=(?:->))get_results(?!.+\bprepare\b)\(.*\)/'						=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
-			'/(?<=(?:->))query(?!.+\bprepare\b)\(.*\)/'											=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
-			'/(?<=(?:->))get_row(?!.+\bprepare\b)\(.*\)/'								=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
-			'/(?<=(?:->))get_var(?!.+\bprepare\b)\(.*\)/'								=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
-			'/(?<=(?:->))get_col(?!.+\bprepare\b)\(.*\)/'								=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
-			'/(?<=->)prepare(?!.*%)/'													=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
-			'/(?<![a-zA-Z])_e\(/'															=> esc_html__( 'All text strings are to be translatable and properly escaped', 'plugin-check' ),
-			'/(?<![a-zA-Z])__\(/'															=> esc_html__( 'All text strings are to be translatable and properly escaped', 'plugin-check' ),
+			'/@(?!media|keyframes|font)(\$|([a-zA-Z]))+/'     		=> esc_html__( 'Possible error suppression is being used', 'plugin-check' ),
+			'/@include/'                                      		=> esc_html__( 'Possible error suppression is being used', 'plugin-check' ),
+			'/@require/'                                      		=> esc_html__( 'Possible error suppression is being used', 'plugin-check' ),
+			'/@file/'                                         		=> esc_html__( 'Possible error suppression is being used', 'plugin-check' ),
+			'/balanceTags\s*\(\s*/'             									=> esc_html__( 'Possible data validation issues found. balanceTags() does not escape data', 'plugin-check' ),
+			'/force_balance_tags\s*\(\s*/'      									=> esc_html__( 'Possible data validation issues found. force_balance_tags() does not escape data', 'plugin-check' ),
+			'/(echo|print)\s*(\$|[a-zA-Z])/' 											=> esc_html__( 'Possible data validation issues found. All dynamic data must be correctly escaped for the context where it is rendered', 'plugin-check' ),
+			'/$_SERVER/'                   												=> esc_html__( 'PHP Global Variable found. Ensure the context is safe and reliable', 'plugin-check' ),
+			'/(?<=(?:->))get_results(?!.+\bprepare\b)\(.*\)/'			=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
+			'/(?<=(?:->))query(?!.+\bprepare\b)\(.*\)/'						=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
+			'/(?<=(?:->))get_row(?!.+\bprepare\b)\(.*\)/'					=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
+			'/(?<=(?:->))get_var(?!.+\bprepare\b)\(.*\)/'					=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
+			'/(?<=(?:->))get_col(?!.+\bprepare\b)\(.*\)/'					=> esc_html__( 'Possible unprepared SQL statements. All queries with this method need to be prepared', 'plugin-check' ),
+			'/(?<=(?:->))prepare\(.*\)/'													=> esc_html__( 'Possible unprepared SQL statements. The prepare method must have placeholders for it to work correctly', 'plugin-check' ),
+			'/(?<![a-zA-Z])_e\(/'																	=> esc_html__( 'All text strings are to be translatable and properly escaped', 'plugin-check' ),
+			'/(?<![a-zA-Z])__\(/'																	=> esc_html__( 'All text strings are to be translatable and properly escaped', 'plugin-check' ),
 		);
 
 		$info_checks = array(
